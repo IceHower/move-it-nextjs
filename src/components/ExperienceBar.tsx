c8c62e6c2323cc9068d0d5bc9d91bc34fa44038d
IceHower <vctelles09@gmail.com>
2021-02-25 02:00:21 -0300

    Create Profile Component

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,10 +1,13 @@
-export function ExperienceBar() {
+import React from 'react';
+import styles from '../styles/components/ExperienceBar.module.css';
+
+export default function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div> 
                 <div style={{width: '60%'}}/>
-                <span className='current-experience' style={{ left: '60%'}}>360 xp</span>
+                <span className={styles.currentExperience} style={{ left: '60%'}}>360 xp</span>
             </div>
             <span>600 xp</span>
         </header>
